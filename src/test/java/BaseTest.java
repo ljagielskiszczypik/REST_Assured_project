17881923b71342f2bab2fe62fcb4efb96f304201
lukas <ljagielskiszczypik@gmail.com>
2026-05-27 20:39:42 +0200

    correct BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,18 +4,16 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import io.restassured.internal.RequestSpecificationImpl;
-import io.restassured.internal.ResponseSpecificationImpl;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
-    protected RequestSpecification reqSpec;
-    protected ResponseSpecification respSpec;
+    protected static RequestSpecification reqSpec;
+    protected static ResponseSpecification respSpec;
 
-    @BeforeClass
+    @BeforeSuite
     public void setUp(){
         reqSpec = new RequestSpecBuilder()
                 .setBaseUri("http://localhost:3000")
--- src/test/java/DeletePostTest.java
+++ src/test/java/DeletePostTest.java
@@ -1,7 +1,6 @@
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class DeletePostTest extends BaseTest{
 
